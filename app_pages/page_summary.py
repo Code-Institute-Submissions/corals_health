b960d533ac359f7b30d04b8b1f022000bc2fc6f4
DrSYakovlev <s.o.yakovlev@gmail.com>
2024-07-26 13:29:02 +0000

    Download and unzip database

--- app_pages/page_summary.py
+++ app_pages/page_summary.py
@@ -1,35 +1,29 @@
 import streamlit as st
-import matplotlib.pyplot as plt
 
+def business_case() -> None:
 
-def page_summary_body():
+    st.write("# Coral\'s Health")
 
-    st.write("### Quick Project Summary")
+    st.write("## Project Summary and business case")
 
     st.info(
-        f"**General Information**\n"
-        f"* Malaria is a parasitic infection transmitted by the bite of infected female "
-        f"Anopheles mosquitoes.\n"
-        f"* A blood smear sample is collected, mixed with a reagent and examined in "
-        f"the microscope. Visual criteria are used to detect malaria parasites.\n"
-        f"* According to [WHO](https://www.who.int/news-room/fact-sheets/detail/malaria), "
-        f"in 2019, there were an estimated  229 million cases of malaria worldwide and an "
-        f"estimated 409 thousand deaths due to this disease. "
-        f"Children <5 years are the most vulnerable group, accounting for 67% (274 thousand) "
-        f"of all malaria deaths worldwide in 2019.\n\n"
-        f"**Project Dataset**\n"
-        f"* The available dataset contains 5643 out of +27 thousand images taken from "
-        f"blood smear workflow (when a drop of blood is taken on a glass slide) of "
-        f"malaria-parasitised and uninfected cells.")
+        f"* Coral bleaching is the process when corals become white due to loss of symbiotic algae and photosynthetic pigments. This loss of pigment can be caused by various stressors, such as changes in ocean temperature (due to Global Warming), light, or nutrients. Bleaching occurs when coral polyps expel the zooxanthellae (dinoflagellates that are commonly referred to as algae) that live inside their tissue, causing the coral to turn white.\n"
+        f"* The zooxanthellae are photosynthetic, and as the water temperature rises, they begin to produce reactive oxygen species. This is toxic to the coral, so the coral expels the zooxanthellae. Since the zooxanthellae produce the majority of coral colouration, the coral tissue becomes transparent, revealing the coral skeleton made of calcium carbonate. Most bleached corals appear bright white, but some are blue, yellow, or pink due to pigment proteins in the coral.\n"
+        f"* The leading cause of coral bleaching is rising ocean temperatures due to climate change caused by anthropogenic activities. A temperature about 1 °C (or 2 °F) above average can cause bleaching.\n"
+        f"* According to the United Nations Environment Programme, between 2014 and 2016, the longest recorded global bleaching events killed coral on an unprecedented scale. In 2016, bleaching of coral on the Great Barrier Reef killed 29 to 50 percent of the reef's coral. In 2017, the bleaching extended into the central region of the reef. The average interval between bleaching events has halved between 1980 and 2016, [Wikipedia article](https://en.wikipedia.org/wiki/Coral_bleaching).\n"        
+        )
+
+    st.write("## Dataset")
+
+    st.info(
+        f"The available dataset contains 1582 images of healthy, bleached and death corals. The dataset is downloadable from [Kaggle](https://www.kaggle.com/datasets/sonainjamil/bhd-corals).\n"
+        )
 
     st.write(
-        f"* For additional information, please visit and **read** the "
-        f"[Project README file](https://github.com/Code-Institute-Solutions/WalkthroughProject01/blob/main/README.md).")
-    
+        f"* Additional information: [Project README file](https://github.com/.../README.md).\n")
 
     st.success(
         f"The project has 2 business requirements:\n"
-        f"* 1 - The client is interested in having a study to differentiate "
-        f"a parasitized and uninfected cell visually.\n"
-        f"* 2 - The client is interested in telling whether a given cell contains a malaria parasite or not. "
+        f"* 1 - The client is interested in having a study to differentiate 'healthy', 'bleached' and 'dead' corals"        
+        f"* 2 - The client is interested to tell whether a given segment/part of coral colony is healthy, suffers from bleaching phenomenon or dead based on the analysis of coral images."
         )
